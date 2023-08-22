14e95e0ecbac1f003e5819abf57f652886f1292d
obie-1 <colin.obadiah@gmail.com>
2023-08-21 23:17:40 -0700

    added url to project1 and removed gallery in index

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,7 +104,7 @@
 					overlayOpacity: 0.85,
 					popupCloserText: '',
 					popupLoaderText: '',
-					selector: '.work-item a.image',
+					selector: '.work-item a.image-popup',
 					usePopupCaption: true,
 					usePopupDefaultStyling: false,
 					usePopupEasyClose: false,
