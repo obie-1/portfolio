42f4396e1cf95b5545caf49030d52a0f76b4f874
obie-1 <colin.obadiah@gmail.com>
2023-08-21 23:24:36 -0700

    added class work-item2 for gallery function

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,5 +113,22 @@
 				});
 
 			});
+			$window.on('load', function() {
+
+				$('#two').poptrox({
+					caption: function($a) { return $a.next('h3').text(); },
+					overlayColor: '#2c2c2c',
+					overlayOpacity: 0.85,
+					popupCloserText: '',
+					popupLoaderText: '',
+					selector: '.work-item2 a.image-popup',
+					usePopupCaption: true,
+					usePopupDefaultStyling: false,
+					usePopupEasyClose: false,
+					usePopupNav: true,
+					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+				});
+
+			});
 
 })(jQuery);
